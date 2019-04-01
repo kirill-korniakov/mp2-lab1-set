c90ef8d3ffcf438bcad93227c49fb9d76b1e242e
Kirill Korniakov <kirill.korniakov@intel.com>
2019-04-01 17:31:33 +0300

    Fix test for den=0

--- src/tratio.cpp
+++ src/tratio.cpp
@@ -1,9 +1,13 @@
 // Реализация класс рационального числа
 
 #include "tratio.h"
+#include <string>
 
 TRatio::TRatio(int num, int den)
 {
+    if (den == 0)
+        throw std::string("Error: can't give zero as denominator!");
+
     this->num = num;
     this->den = den;
 }
